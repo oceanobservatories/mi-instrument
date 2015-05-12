611db5bda7366fe0f11e7a3f431ce191fc4ec6e1
Rachel Manoni <rachel.manoni@gmail.com>
2015-05-12 13:40:31 -0700

    Issue #3070 PREST Error reported by the instrument when a settime comand was sent everything was changed, good luck reviewing this
    removed empty implementation seabird/sbe54tps/ooicore
    removed unused class seabird/sbe37smb/
    
    Change-Id: I46f4d09de59fe5d9a2c72e11247370d599e1134b

--- mi/instrument/seabird/sbe54tps/ooicore/driver.py
+++
@@ -1,6 +0,0 @@
-from mi.instrument.seabird.sbe54tps.driver import SBE54PlusInstrumentDriver
-
-class InstrumentDriver(SBE54PlusInstrumentDriver):
-    """
-    Specialization for this version of the 54 driver
-    """
