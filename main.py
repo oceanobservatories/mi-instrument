fed300c6ffd8efacc91a2f2eaa0e75798d23c725
Peter Cable <petercable@gmail.com>
2015-07-20 07:30:16 -0700

    .
    Change-Id: Iac7fc2b4a4b06b7c303afaba12b17ed9b219b91d

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import sys
-from mi.core.instrument.wrapper import ZmqDriverProcess
+from mi.core.instrument.wrapper import main
 
-if __name__ == '__main__':
-    module = sys.argv[1]
-    klass = sys.argv[2]
-    command_port = int(sys.argv[3])
-    event_port = int(sys.argv[4])
-    dp = ZmqDriverProcess(module, klass, command_port, event_port)
-    dp.run()
+main()
