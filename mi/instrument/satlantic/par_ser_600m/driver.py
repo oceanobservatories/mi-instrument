75b21eb0dde250886634fc32befa77520b73d3df
Jeremy Amundson <Jeremy.S.Amundson@raytheon.com>
2016-03-24 08:37:08 -0400

    added timeout to PARAD's discover state
    Change-Id: I52402c166364d4661c4194cc99f60164f805f890

--- mi/instrument/satlantic/par_ser_600m/driver.py
+++ mi/instrument/satlantic/par_ser_600m/driver.py
@@ -458,7 +458,7 @@ class SatlanticPARInstrumentProtocol(CommandResponseInstrumentProtocol):
         self._cmd_dict.add(PARCapability.ACQUIRE_STATUS, display_name='Acquire Status')
         self._cmd_dict.add(PARCapability.START_AUTOSAMPLE, display_name='Start Autosample')
         self._cmd_dict.add(PARCapability.STOP_AUTOSAMPLE, display_name='Stop Autosample')
-        self._cmd_dict.add(PARCapability.DISCOVER, display_name='Discover')
+        self._cmd_dict.add(PARCapability.DISCOVER, display_name='Discover', timeout=30)
 
     def _build_driver_dict(self):
         """
