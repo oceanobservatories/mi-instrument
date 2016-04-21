5a698e3a33ab5658d5c08deda97b01aad45e4265
Rene Gelinas <rene.p.gelinas@gmail.com>
2016-04-21 16:19:22 -0700

    Added OMS Alert Alarm Server and config file to service the OMS posting OMS Events containing Alert Alarm messages.
    Change-Id: Id662ee72f2b198fc21775081361490da1e65fd2c

---
+++ mi/platform/rsn/oms_alert_alarm_server.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+"""
+@brief Web-based server, servicing Alerts & Alarms events posted by the
+OMS.  This server will register itself as a listener to the OMS Server.
+It will publish OMS Events containing Alert & Alarm messages to the
+qpid queue specified in the configuration file:
+oms_alert_alarm_server.cfg.
+
+Usage:
+    oms_alert_alarm_server <server_config>
+
+Options:
+    -h, --help          Show this screen.
+
+"""
+
+import re
+import json
+import xmlrpclib
+import yaml
+from docopt import docopt
+from flask import Flask, request
+from mi.core.instrument.publisher import Publisher
+from mi.core.log import LoggerManager
+from ooi.logging import log
+import httplib
+
+app = Flask(__name__)
+aa_publisher = None
+
+LoggerManager()
+log.setLevel('INFO')
+
+@app.route('/', methods=['POST'])
+def process_oms_request():
+    """
+    This is the method that is called when the OMS POSTs OMS Events to
+    this registered listener at the "/" path.
+    :return:
+    """
+
+    if len(request.json) != 0:
+        # Log the list of Alert & Alarm messages from the OMS Event
+        for alert_alarm_dict in request.json:
+            log.info('oms_alert_alarm_server: OMS_AA_MSG: %r',
+                     alert_alarm_dict)
+
+        # Publish the list of Alert & Alarm messages to qpid
+        aa_publisher.publish(request.json)
+
+    else:
+        log.error('No data in the POSTed alert/alarm OMS Event ...')
+
+    return '', httplib.ACCEPTED
+
+
+def start_web_service(oms_uri, alert_alarm_server_uri):
+    """
+    This method gets the proxy for the OMS Server, registers this server
+    as a listener to the OMS and starts the Flask web service that will
+    listen for OMS Events from the OMS Server.
+    :param oms_uri: The URI of the OMS Server
+    :param alert_alarm_server_uri: The URI of this server.
+    :return:
+    """
+
+    alert_alarm_server_port = int(re.search('http://.+:(.+?)/',
+                                            alert_alarm_server_uri).group(1))
+
+    if oms_uri == 'DEBUG':
+        log.info('DEBUG mode: OMS Alert Alarm Server not registering ' +
+                 'with OMS.')
+    else:
+        log.info('Getting the proxy for OMS server: ' + oms_uri)
+        proxy = xmlrpclib.ServerProxy(oms_uri)
+
+        log.info('Registering OMS Alerts & Alarms server as listener: ' +
+                 alert_alarm_server_uri)
+        proxy.event.register_event_listener(alert_alarm_server_uri)
+
+    log.info('Listening for Alerts & Alarms on ' + alert_alarm_server_uri)
+    app.run(host='0.0.0.0', port=alert_alarm_server_port)
+
+
+def main():
+    """
+    This main routine will get the configuration file from the command
+    line parameter and set the values for required URIs for the OMS, the
+    OMS Alert Alarm Server and the qpid Server.  It will then get the qpid
+    publisher for publishing the OMS events.  Finally, it will start the web
+    service.
+    """
+
+    global aa_publisher
+
+    oms_uri = None
+    alert_alarm_server_uri = None
+    qpid_uri = None
+
+    options = docopt(__doc__)
+    server_config_file = options['<server_config>']
+    config = yaml.load(open(server_config_file))
+
+    oms_uri = config.get('oms_uri')
+    alert_alarm_server_uri = config.get('alert_alarm_server_uri')
+    qpid_uri = config.get('qpid_uri')
+
+    if not all((oms_uri, alert_alarm_server_uri, qpid_uri)):
+        log.error('Missing mandatory configuration values missing from ' +
+                  server_config_file)
+    else:
+        headers = {'aaServerUri': alert_alarm_server_uri}
+
+        try:
+            aa_publisher = Publisher.from_url(qpid_uri, headers)
+            start_web_service(oms_uri, alert_alarm_server_uri)
+
+        except Exception as ex:
+            log.exception('Error starting OMS Alert and Alarm web service: %r',
+                          ex)
+
+    log.info('Stopping OMS Alert & Alarm Web Service.\n')
+
+if __name__ == '__main__':
+    main()
