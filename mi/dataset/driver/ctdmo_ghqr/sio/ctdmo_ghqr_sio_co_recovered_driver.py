a66e1a6043d70a6cf45b3882003055189f528a9f
vipul <vipulraytheon@gmail.com>
2017-05-04 16:09:55 -0700

    Update ctdmo_ghqr_sio_co_recovered_driver.py

--- mi/dataset/driver/ctdmo_ghqr/sio/ctdmo_ghqr_sio_co_recovered_driver.py
+++ mi/dataset/driver/ctdmo_ghqr/sio/ctdmo_ghqr_sio_co_recovered_driver.py
@@ -14,7 +14,7 @@ from mi.dataset.parser.ctdmo_ghqr_sio import CtdmoGhqrSioRecoveredCoAndCtParser
 from mi.core.versioning import version
 
 
-@version("15.6.1")
+@version("15.7.0")
 def parse(unused, source_file_path, particle_data_handler):
     """
     This is the method called by Uframe
