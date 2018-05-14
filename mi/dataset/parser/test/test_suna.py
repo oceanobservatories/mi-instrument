ed7e93902da3185f0e8ac8096694606802f355a6
Phil Tran <philtrann@gmail.com>
2018-05-14 14:13:12 -0700

    Compressed duplicate test functions

--- mi/dataset/parser/test/test_suna.py
+++ mi/dataset/parser/test/test_suna.py
@@ -7,10 +7,11 @@ from mi.dataset.driver.suna.resource import RESOURCE_PATH
 from mi.dataset.test.test_parser import ParserUnitTestCase
 from mi.dataset.dataset_parser import DataSetDriverConfigKeys
 from mi.dataset.parser.suna import SunaParser
-
-from mi.dataset.driver.suna.suna_driver import MODULE_NAME, RECOVERED_PARTICLE_CLASS, INSTRUMENT_RECOVERED_PARTICLE_CLASS
+from mi.dataset.driver.suna.suna_driver import MODULE_NAME, RECOVERED_PARTICLE_CLASS, \
+    INSTRUMENT_RECOVERED_PARTICLE_CLASS
 
 log = get_logger()
+
 
 @attr('UNIT', group='mi')
 class SunaDclParserUnitTestCase(ParserUnitTestCase):
@@ -26,80 +27,49 @@ class SunaDclParserUnitTestCase(ParserUnitTestCase):
             self.exception_callback)
         return parser
 
-    def open_file(self, filename):
+    @staticmethod
+    def open_file(filename):
         my_file = open(os.path.join(RESOURCE_PATH, filename), mode='r')
         return my_file
+
+    @staticmethod
+    def create_yml(particles, filename):
+        particle_to_yml(particles, os.path.join(RESOURCE_PATH, filename))
 
     def setUp(self):
         ParserUnitTestCase.setUp(self)
 
-    def create_yml(self, particles, filename):
-        particle_to_yml(particles, os.path.join(RESOURCE_PATH, filename))
+    def assert_particles_processed(self, filename, particle_class, num_particles):
+        """
+        Common function to test particles
+        """
+        with open(os.path.join(RESOURCE_PATH, filename), 'r') as file_handle:
+            parser = self.create_parser(particle_class, file_handle)
+            particles = parser.get_records(num_particles + 10)
+            log.debug("Num particles: %d", len(particles))
+
+            self.assertEqual(len(particles), num_particles)
 
     def test_good_dcl(self):
         """
         Test dcl with good data
         """
-        log.debug("Running test_happy_path")
-
-        filename = "dcl_good.log"
-
-        with open(os.path.join(RESOURCE_PATH, filename), 'r') as file_handle:
-
-            parser = self.create_parser(RECOVERED_PARTICLE_CLASS, file_handle)
-
-            particles = parser.get_records(1000)
-            log.debug("Num particles: %d", len(particles))
-
-            self.assertEqual(len(particles), 144)
+        self.assert_particles_processed('dcl_good.log', RECOVERED_PARTICLE_CLASS, 144)
 
     def test_bad_dcl(self):
         """
         Test dcl with some good data, and some bad data (4 of them)
         """
-        log.debug("Running test_happy_path")
-
-        filename = "dcl_bad.log"
-
-        with open(os.path.join(RESOURCE_PATH, filename), 'r') as file_handle:
-
-            parser = self.create_parser(RECOVERED_PARTICLE_CLASS, file_handle)
-
-            particles = parser.get_records(1000)
-            log.debug("Num particles: %d", len(particles))
-
-            self.assertEqual(len(particles), 140)
+        self.assert_particles_processed('dcl_bad.log', RECOVERED_PARTICLE_CLASS, 140)
 
     def test_good_instrument_recovered(self):
         """
         Test instrument recovered with some good data
         """
-        log.debug("Running test_happy_path")
-
-        filename = "instrument_recovered_good.CSV"
-
-        with open(os.path.join(RESOURCE_PATH, filename), 'r') as file_handle:
-
-            parser = self.create_parser(INSTRUMENT_RECOVERED_PARTICLE_CLASS, file_handle)
-
-            particles = parser.get_records(1000)
-            log.debug("Num particles: %d", len(particles))
-
-            self.assertEqual(len(particles), 39)
+        self.assert_particles_processed('instrument_recovered_good.CSV', INSTRUMENT_RECOVERED_PARTICLE_CLASS, 39)
 
     def test_bad_instrument_recovered(self):
         """
         Test instrument recovered with some bad data (
         """
-        log.debug("Running test_happy_path")
-
-        filename = "instrument_recovered_bad.CSV"
-
-        with open(os.path.join(RESOURCE_PATH, filename), 'r') as file_handle:
-
-            parser = self.create_parser(INSTRUMENT_RECOVERED_PARTICLE_CLASS, file_handle)
-
-            particles = parser.get_records(1000)
-            log.debug("Num particles: %d", len(particles))
-
-            self.assertEqual(len(particles), 35)
+        self.assert_particles_processed('instrument_recovered_bad.CSV', INSTRUMENT_RECOVERED_PARTICLE_CLASS, 35)
