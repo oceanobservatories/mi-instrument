db24b6195385a2701ba388622a8603ec74adcf61
oceanzus <case@oceanz.org>
2022-03-18 01:36:24 -0400

    Added Antelope 5 minute FLAC file modification.

--- mi/instrument/antelope/orb/ooicore/packet_log.py
+++ mi/instrument/antelope/orb/ooicore/packet_log.py
@@ -6,6 +6,11 @@ from datetime import datetime
 from obspy.core import Stats
 import numpy as np
 from obspy import Trace
+from obspy.core.utcdatetime import UTCDateTime
+
+import soundfile as sf
+import json
+import io
 
 from mi.core.log import get_logger
 from mi.core.exceptions import InstrumentProtocolException
@@ -46,12 +51,13 @@ class GapException(Exception):
 
 
 class PacketLogHeader(object):
-    def __init__(self, net, location, station, channel, starttime, maxtime, rate, calib, calper, refdes):
+    def __init__(self, net, location, station, channel, starttime, mintime, maxtime, rate, calib, calper, refdes):
         self.net = net
         self.location = location
         self.station = station
         self.channel = channel
         self.starttime = starttime
+        self.mintime = mintime
         self.maxtime = maxtime
         self.rate = rate
         self.calib = calib
@@ -61,7 +67,7 @@ class PacketLogHeader(object):
 
     @property
     def time(self):
-        return self._format_time(self.starttime)
+        return self._format_time(self.mintime)
 
     @staticmethod
     def _format_time(timestamp):
@@ -70,6 +76,7 @@ class PacketLogHeader(object):
         # add them back in if they are missing for consistency in file naming
         if t[-3] == ':':
             t += '.000000'
+        t += 'Z'
         return t
 
     @property
@@ -77,8 +84,12 @@ class PacketLogHeader(object):
         return '-'.join((self.net, self.station, self.location, self.channel))
 
     @property
+    def file_format(self):
+        return 'flac'
+
+    @property
     def fname(self):
-        return '%s-%s.mseed' % (self.name, self.time)
+        return '%s-%s.%s' % (self.name, self.time, self.file_format)
 
     @property
     def stats(self):
@@ -120,8 +131,8 @@ class PacketLog(object):
         # Generate a UUID for this PacketLog
         self.bin_uuid = str(uuid.uuid4())
 
-    def create(self, net, location, station, channel, start, end, rate, calib, calper, refdes):
-        self.header = PacketLogHeader(net, location, station, channel, start, end, rate, calib, calper, refdes)
+    def create(self, net, location, station, channel, start, mintime, maxtime, rate, calib, calper, refdes):
+        self.header = PacketLogHeader(net, location, station, channel, start, mintime, maxtime, rate, calib, calper, refdes)
         if not os.path.exists(self.abspath):
             try:
                 os.makedirs(self.abspath)
@@ -131,7 +142,7 @@ class PacketLog(object):
             raise InstrumentProtocolException('Error creating file path: File exists with same name: ' + self.abspath)
 
     @staticmethod
-    def from_packet(packet, end, refdes):
+    def from_packet(packet, bin_start, bin_end, refdes):
         packet_log = PacketLog()
         packet_log.create(
             packet.get('net', ''),
@@ -139,7 +150,8 @@ class PacketLog(object):
             packet.get('sta', ''),
             packet.get('chan', ''),
             packet['time'],
-            end,
+            bin_start,
+            bin_end,
             packet['samprate'],
             packet['calib'],
             packet['calper'],
@@ -151,7 +163,7 @@ class PacketLog(object):
     def relpath(self):
         if self._relpath is None:
             # Get the year, month and day for the directory structure of the data file from the packet start time
-            packet_start_time = datetime.utcfromtimestamp(self.header.starttime)
+            packet_start_time = datetime.utcfromtimestamp(self.header.mintime)
             year = str(packet_start_time.year)
             month = '%02d' % packet_start_time.month
             day = '%02d' % packet_start_time.day
@@ -171,13 +183,8 @@ class PacketLog(object):
         return os.path.join(self.base_dir, self.header.refdes, self.relname)
 
     def add_packet(self, packet):
-        if self.header.starttime > packet['time'] or packet['time'] >= self.header.maxtime:
-            raise GapException()
-
-        # check if there is a gap, if so reject this packet
-        diff = abs(self.header.endtime - packet['time'])
-        maxdiff = packet['nsamp'] * self.header.delta * self.TIME_FUDGE_PCNT / 100
-        if diff > maxdiff:
+        # If the current packet is not within the 5 min file, then move to the next 5 minute file
+        if self.header.mintime > packet['time'] or packet['time'] >= self.header.maxtime:
             raise GapException()
 
         # split this packet if necessary
@@ -201,8 +208,68 @@ class PacketLog(object):
         self.needs_flush = True
 
     def _write_trace(self):
+        log.info('Hydrophone data rate: %s' % str(self.header.rate))
+
+        # Store the Orb data to an ObsPy Trace, trim to 5 minutes and pad with zeros
         trace = Trace(self.data.get(), self.header.stats)
-        trace.write(self.absname, format='MSEED')
+        trace_trimmed_padded = trace.copy()
+        trace_trimmed_padded.trim(starttime=UTCDateTime(self.header.mintime),
+                                  endtime=UTCDateTime(self.header.maxtime),
+                                  pad=True,
+                                  nearest_sample=True,
+                                  fill_value=0)
+
+        # Write the trimmed and padded FLAC file and JSON metadata to disk
+        flac_trimmed_padded_filename = self.absname
+        with sf.SoundFile(flac_trimmed_padded_filename, 'w', int(self.header.rate), 1, 'PCM_24') as f:
+            # trace.data is in int32, Soundfile native is float64, so convert it
+            trace_float = trace_trimmed_padded.data / ((2.0 ** 23) - 1)
+            f.write(trace_float)
+        flac_trimmed_padded_filename_json = flac_trimmed_padded_filename + '.json'
+        with io.open(flac_trimmed_padded_filename_json, 'w', encoding='utf-8') as f:
+            file_stats = sf.info(flac_trimmed_padded_filename, verbose=True)
+            file_stats_dict = file_stats.__dict__
+            file_stats_clean = json.dumps({key: value for key, value in file_stats_dict.items()}, indent=4, sort_keys=True, default=str)
+            f.write(unicode(file_stats_clean))
+        log.info('Wrote 5 minute, zero-padded FLAC file to disk: %s' % flac_trimmed_padded_filename)
+
+        # TODO: Remove this block after testing
+        # Write the MSEED file to disk
+        trace.write(self.absname.replace('.flac', '_orig.mseed'), format='MSEED')
+        mseed_filename_json = self.absname.replace('.flac', '_orig.mseed') + '.json'
+        with io.open(mseed_filename_json, 'w', encoding='utf-8') as f:
+            file_stats = trace.stats
+            file_stats_dict = file_stats.__dict__
+            file_stats_clean = json.dumps({key: value for key, value in file_stats_dict.items()}, indent=4, sort_keys=True, default=str)
+            f.write(unicode(file_stats_clean))
+        log.info('Wrote original Trace MSEED file to disk: %s' % self.absname.replace('.flac', '_orig.mseed'))
+
+        # TODO: Remove this block after testing
+        # Write the trimmed and padded MSEED file to disk
+        trace_trimmed_padded_filename = self.absname.replace('.flac', '.mseed')
+        trace_trimmed_padded.write(trace_trimmed_padded_filename, format='MSEED')
+        mseed_trimmed_padded_filename_json = trace_trimmed_padded_filename + '.json'
+        with io.open(mseed_trimmed_padded_filename_json, 'w', encoding='utf-8') as f:
+            file_stats = trace_trimmed_padded.stats
+            file_stats_dict = file_stats.__dict__
+            file_stats_clean = json.dumps({key: value for key, value in file_stats_dict.items()}, indent=4, sort_keys=True, default=str)
+            f.write(unicode(file_stats_clean))
+        log.info('Wrote trimmed and zero padded MSEED file to disk: %s' % trace_trimmed_padded_filename)
+
+        # TODO: Remove this block after testing
+        # Write the FLAC file to disk
+        flac_filename = self.absname.replace('.flac', '_orig.flac')
+        with sf.SoundFile(flac_filename, 'w', int(self.header.rate), 1, 'PCM_24') as f:
+            # trace.data is in int32, Soundfile native is float64, so convert it
+            trace_float = trace.data / ((2.0 ** 23) - 1)
+            f.write(trace_float)
+        flac_filename_json = flac_filename + '.json'
+        with io.open(flac_filename_json, 'w', encoding='utf-8') as f:
+            file_stats = sf.info(flac_filename, verbose=True)
+            file_stats_dict = file_stats.__dict__
+            file_stats_clean = json.dumps({key: value for key, value in file_stats_dict.items()}, indent=4, sort_keys=True, default=str)
+            f.write(unicode(file_stats_clean))
+        log.info('Wrote original Trace FLAC file to disk: %s' % flac_filename)
 
     def flush(self):
         if self.needs_flush:
